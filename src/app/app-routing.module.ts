376ac8b2b5520c0c604ff6194333304b60243b40
Eduard Maestro <emaestromartinez@gmail.com>
2020-07-24 20:09:58 +0200

    Added Routing. Created three main components: dashboard, movies and characters.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CharactersComponent } from './characters/characters.component';
+import { MoviesComponent } from './movies/movies.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'characters', component: CharactersComponent },
+  { path: 'movies', component: MoviesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
